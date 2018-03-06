048688dd30f83d95afd739f5a682692866ad9891
NoriakiHoriuchi <youpsit@gmail.com>
2018-03-06 23:42:01 +0900

    add comments

---
+++ ff.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+## Fast Forward
+
+# Check if the Heroku account exists.
+
+
+# Check if the GitHub account exists.
+
+
+# Check if the project name is available at Heroku.
+
+
+# Check if the project name is available at GitHub.
+
+
+# Create project at Heroku.
+
+
+# Create repository at GitHub.
+
+
+# Create a new Play Framework project with sbt.
+
+
+# Initialize as a new Git repository.
+
+
+# Push to GitHub.
+
+
+# Push to Heroku.
+
